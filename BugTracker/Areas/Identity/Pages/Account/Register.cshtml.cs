5467f72e1b43963c05fbcd077a1abf4ba49a3314
Marco <stari.marco11@gmail.com>
2021-08-26 17:22:29 +0200

    Styled login and register page

--- BugTracker/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BugTracker/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -65,10 +65,10 @@ namespace BugTracker.Areas.Identity.Pages.Account
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
+            //[DataType(DataType.Password)]
+            //[Display(Name = "Confirm Password")]
+            //[Compare("Password", ErrorMessage = "The password and confirmation password don't match.")]
+            //public string ConfirmPassword { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
