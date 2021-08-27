2770cfa18058160882b6c5d8a971f3b263cf6657
Marco <stari.marco11@gmail.com>
2021-08-27 19:06:07 +0200

    Projects page UI

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BugTracker.Models;
+using Microsoft.AspNetCore.Authorization;
+using BugTracker.Data;
 
 namespace BugTracker.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
