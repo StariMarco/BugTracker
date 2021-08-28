19f510dba7f16ce81e4be24a5cb368276d4265df
Marco <stari.marco11@gmail.com>
2021-08-28 14:17:19 +0200

    Implemented sidenav

---
+++ BugTracker/Controllers/ProjectController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BugTracker.Data;
+using BugTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BugTracker.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProjectController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index(int id)
+        {
+            Project project = _db.Projects.Find(id);
+
+            return View(project);
+        }
+    }
+}
